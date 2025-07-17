eddab9cfbf4db866b08ae42dd1a04f5ab86d3c7c
gabrieltorol <gabriel@robotipy.com>
2025-07-16 20:44:34 -0400

    dummy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: process.env.NODE_ENV === 'production' ? '/landing-rabbit2/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
